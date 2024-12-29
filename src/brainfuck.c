082497f3c34d89cc3c865659226dbda40a3d569c
kunimalius <yu.kuni.maru@gmail.com>
2024-12-29 11:21:02 +0900

    プロンプトを表示するように修正

--- src/brainfuck.c
+++ src/brainfuck.c
@@ -6,10 +6,12 @@ int main() {
         char buf[256] = {'\0'};
         BFInterpreter *interpreter;
         init_interpreter(&interpreter);
+        printf("$");
         while (scanf("%s", buf) > 0) {
                 set_instruction(interpreter, buf);
                 run(interpreter);
                 memset(buf, 0, sizeof(char) * 256);
+                printf("\n$");
         }
 
         return 0;
